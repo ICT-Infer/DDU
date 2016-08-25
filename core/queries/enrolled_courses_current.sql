fa8c99e02f2d219edb4aab1a423054ccf717247a
Erik Nordstrøm <erik@nordstroem.no>
2016-08-25 17:31:54 +0200

    Replace hard-coded values for year and autumn with use of PostgreSQL functions to determine the year and whether or not it is currently the autumn semester.

--- core/queries/enrolled_courses_current.sql
+++ core/queries/enrolled_courses_current.sql
@@ -9,6 +9,9 @@ inner join institutions
 	on courses.institution = institutions.id
 where
 	person = '1'
-	and courses.year = '2016'
-	and courses.autumn
+	and courses.year = (select extract(year from now()))
+	-- If the current month is >= 7, then we are in or after july,
+	-- which means that it is the autumn semester. Otherwise, it is
+	-- the spring semester as denoted by autumn being false.
+	and courses.autumn = (select extract(month from now()) >= 7)
 order by courses.code;
