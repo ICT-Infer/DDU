8ed82a6713e7e14c5cad8374f0c78d5e3682e9e3
Erik Nordstrøm <erik@nordstroem.no>
2016-09-22 14:38:02 +0200

    Complex list.

---
+++ couchdb/homework/_design/status/_list/complex.js
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2016 Erik Nordstrøm <erikn@ict-infer.no>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+function(head, req)
+{
+	provides("html", function ()
+	{
+		html = "";
+
+		function trv_pending_delivery (vd)
+		{
+			html += "<td>" + vd.due_date + "</td>";
+
+			html += "<td><date datetime=\""
+				+ vd.due_ts + "\">"
+				+ vd.due_zclock + "</a></td>";
+		}
+
+		function trv_pending_review (vd)
+		{
+			html += "<td>" + vd.delivered_date + "</td>";
+
+			html += "<td><date datetime=\""
+				+ vd.delivered_ts + "\">"
+				+ vd.delivered_zclock + "</a></td>";
+		}
+
+		function trv_approved (vd)
+		{
+			html += "<td>" + vd.score + "</td>";
+		}
+
+		function tr (trv, row)
+		{
+			html += "<tr>";
+
+			vd = row.value; // view-emitted data values
+
+			html += "<td><a href=\"" + vd.course_url + "\">"
+				+ vd.course + "</a></td>";
+
+			html += "<td>" + vd.title + "</td>";
+
+			trv(vd);
+
+			html += "</tr>";
+		}
+
+		function table (first_row, n, title, trv)
+		{
+			row = first_row;
+
+			if (row && ('key' in row) && row.key[0] === n)
+			{
+				html += "<h2>" + title + "</h2>";
+				html += "<table><tbody>";
+
+				do
+				{
+					tr(trv, row);
+					row = getRow();
+				} while (row && ('key' in row) && row.key[0] === n)
+
+				html += "</tbody></table>";
+			}
+
+			return row;
+		}
+
+		row = getRow();
+		row = table(row, 0, "Pending delivery", trv_pending_delivery);
+		row = table(row, 1, "Pending review", trv_pending_review);
+		row = table(row, 2, "Approved", trv_approved);
+
+		return html;
+	});
+}
